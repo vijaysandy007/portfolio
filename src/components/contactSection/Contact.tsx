2303e5d25e6d4bdf1e4ff1f8ad043a2edfe55889
vijayp <vijayp@nextazy.com>
2025-03-04 18:48:22 +0530

    animation added

--- src/components/contactSection/Contact.tsx
+++ src/components/contactSection/Contact.tsx
@@ -1,40 +1,71 @@
 
 import "./Contact.scss";
+import Resume from "../../assets/files/resume.pdf";
+import { useEffect, useRef } from "react";
+import { ToastContainer, toast } from 'react-toastify';
 const Contact = () => {
+  const downloadRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const handleDownload = (e: Event) => {
+      e.preventDefault();
+      const link = document.createElement("a");
+      link.href = Resume;
+      link.download = "resume.pdf";
+      link.click();
+      toast.success("Resume downloaded successfully!",{
+        theme: "dark"
+      });
+    };
+
+    const button = downloadRef.current;
+    if (button) {
+      button.addEventListener("click", handleDownload);
+    }
+
+    return () => {
+      if (button) {
+        button.removeEventListener("click", handleDownload);
+      }
+    };
+  }, []);
+
+
   return (
     <>
-    <div className="contact-section-container">
-    <h2>Contact</h2>
+      <div className="contact-section-container block">
+        <h2>Contact</h2>
 
-      <div className="group">
-                <div className="text">
-                    Looking for a skilled Full Stack Developer to join your team? Or want to discuss an exciting
-                    project? I’m open to job opportunities, freelance work, and collaborations!
+        <div className="group">
+          <div className="text">
+            Looking for a skilled Full Stack Developer to join your team? Or want to discuss an exciting
+            project? I’m open to job opportunities, freelance work, and collaborations!
 
-                    With <b> 3+ years of experience </b>, I specialize in building scalable web applications, crafting
-                    high-performance APIs, and working with modern frontend & backend technologies. Whether you're a
-                    recruiter, a company, or an entrepreneur, I'd love to connect and explore how I can contribute to
-                    your team or project.
-                </div>
+            With <b> 3+ years of experience </b>, I specialize in building scalable web applications, crafting
+            high-performance APIs, and working with modern frontend & backend technologies. Whether you're a
+            recruiter, a company, or an entrepreneur, I'd love to connect and explore how I can contribute to
+            your team or project.
+          </div>
 
-                <form>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name"/>
+          <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
 
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email"/>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
 
-                    <label htmlFor="message">Message</label>
-                    <textarea id="message" cols={30} rows={10}></textarea>
+            <label htmlFor="message">Message</label>
+            <textarea id="message" cols={30} rows={10}></textarea>
 
-                    <div className="button-group">
-                    <button type="submit">Send Message</button>
-                    <button type="submit">Download CV</button>
+            <div className="button-group">
+              <button type="submit">Send Message</button>
+              <button ref={downloadRef} type="button">Download CV</button>
+              <ToastContainer />
 
-                    </div>
-                </form>
             </div>
-    </div>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,6 +3,7 @@ import HeroSection from "../HeroSection/HeroSection";
 import SkillSection from "../SkillSection/SkillSection";
 import ProjectSection from "../projectSection/ProjectSection";
 import Contact from "../contactSection/Contact";
+import { motion } from "framer-motion";
 import "./Layout.scss";
 const Layout = () => {
     return (
@@ -11,18 +12,54 @@ const Layout = () => {
                 <Header />
             </nav>
             <main>
-                <section className="hero-section">
-                    <HeroSection/>
-                </section>
-                <section id="skills" className="skills-section">
-                    <SkillSection/>
-                </section>
-                <section id="projects" className="projects-section">
-                    <ProjectSection/>
-                </section>
-                <section id="contact" className="contact-section">
-                    <Contact/>
-                </section>
+
+                <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6 }}
+                    viewport={{ once: true, amount: 0.5 }}
+                >
+                    <section className="hero-section">
+                        <HeroSection />
+                    </section>
+                </motion.div>
+
+                {/* SKILL SECTION */}
+                <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                    viewport={{ once: true, amount: 0.5 }}
+                >
+                    <section id="skills" className="skills-section block">
+                        <SkillSection />
+                    </section>
+                </motion.div>
+                {/* SKILL SECTION END*/}
+
+
+                <motion.div 
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6 }}
+                    viewport={{ once: false, amount: 0.5 }}
+                >
+                    <section id="projects" className="projects-section">
+                        <ProjectSection  />
+                    </section>
+                </motion.div>
+
+
+                <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                    viewport={{ once: true, amount: 0.5 }}
+                >
+                    <section id="contact" className="contact-section">
+                        <Contact />
+                    </section>
+                </motion.div>
             </main>
         </>
 
